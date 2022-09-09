3ae28786ff802f422ed5aa93b1ec0b906bc5020f
mykim <mykim8710@gmail.com>
2022-09-09 19:47:02 +0900

    [ dev ] 국제화 - 유저가 선택하도록 수정 - @Configuration정의 : 메세지, 국제화 설정

--- src/test/java/com/example/itemservice/message/MessageSourceTest.java
+++ src/test/java/com/example/itemservice/message/MessageSourceTest.java
@@ -20,7 +20,7 @@ class MessageSourceTest {
     @Test
     @DisplayName("메세지 - message Test")
     void helloMessageTest() {
-        Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
+        //Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
         String result = ms.getMessage("hello", null, null);
         assertThat(result).isEqualTo("안녕");
     }
@@ -28,14 +28,14 @@ class MessageSourceTest {
     @Test
     @DisplayName("메세지 - message code가 없을때 Exception 발생 Test")
     void notFoundMessageCodeTest() {
-        Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
+        //Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
         assertThatThrownBy(() -> ms.getMessage("no_code", null, null)).isInstanceOf(NoSuchMessageException.class);
     }
 
     @Test
     @DisplayName("메세지 - message code가 없을때 기본메세지 설정 Test")
     void notFoundMessageCodeDefaultMessageTest() {
-        Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
+        //Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
         String result = ms.getMessage("no_code", null, "기본 메시지", null);
         assertThat(result).isEqualTo("기본 메시지");
     }
@@ -43,7 +43,7 @@ class MessageSourceTest {
     @Test
     @DisplayName("메세지 - 매개변수 Test")
     void argumentMessageTest() {
-        Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
+        //Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
         String result = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
         assertThat(result).isEqualTo("안녕 Spring");
     }
@@ -51,7 +51,7 @@ class MessageSourceTest {
     @Test
     @DisplayName("국제화 - default language Test")
     void defaultLangTest() {
-        Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
+        //Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
         assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
         assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
     }
@@ -59,7 +59,7 @@ class MessageSourceTest {
     @Test
     @DisplayName("국제화 - US language Test")
     void enLangTest() {
-        Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
+        //Locale.setDefault(Locale.KOREA);    // 시스템 default가 us로 되어있음
         assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
     }
 }
